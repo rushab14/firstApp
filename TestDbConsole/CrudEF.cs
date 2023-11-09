9b6348981e0a144109df0a911ace900d3112b925
rushab14 <rushabpk31@gmail.com>
2023-11-09 14:13:20 +0530

    crud method class created

---
+++ TestDbConsole/CrudEF.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TESTDAL;
+namespace TestDbConsole
+{
+    public class CrudEF<T> where T :Parent
+    {
+        static TestDBContext dBContext = new TestDBContext();
+        public static void Add(string pName,bool active) {
+            dBContext.Parents.Add(new Parent() { name = pName, isActive = active });
+            dBContext.SaveChanges();
+        }
+        public static void Update(string pname, string tbupdated) {
+         var toBeUpdated = dBContext.Parents.ToList()
+    .Where(parent => parent.name == pname)
+    .FirstOrDefault();
+toBeUpdated.name = tbupdated;
+dBContext.SaveChanges();
+        }
+
+        public static void Delete(string pname, string tbdelted) {
+            var toBedeletedd = dBContext.Parents.ToList()
+        .Where(parent => parent.name == pname)
+        .FirstOrDefault();
+            dBContext.Parents.Remove(toBedeletedd);
+            dBContext.SaveChanges();
+
+        }
+        public static List<T> ? Get() {
+
+            return dBContext.Parents.ToList()  as List<T>;
+        }
+        public static T ? SearchOne(string pname) { 
+        
+        
+        var result = dBContext.Parents
+                .ToList()
+                .Where(p => p.name == pname)
+                .FirstOrDefault();
+
+            return result as T;
+        }
+
+
+
+    }
+}
---
+++ TestDbConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TESTDAL;
+using TestDbConsole;
+
+//TestDBContext dBContext = new TestDBContext();
+//if (false)
+//{
+//    dBContext.Parents.Add(new Parent() { name = "Imposter", isActive = false });
+//    dBContext.SaveChanges();
+//}
+
+//Console.WriteLine(CrudEF<Parent>.Get());
+//Console.WriteLine(CrudEF<Child>.Get());
+//var toBeUpdated = dBContext.Parents.ToList()
+//    .Where(parent => parent.name == "Ankit")
+//    .FirstOrDefault();
+//toBeUpdated.name = "Syed";
+//dBContext.SaveChanges();
+
+//CrudEF<Parent>.Delete("Imposter","toBeDeletedd");
+//CrudEF<Parent>.Add("Shivam", true);
+//CrudEF<Parent>.Add("Ankit", false);
+//CrudEF<Parent>.Update("Imposter", "Ismail");
+Console.WriteLine(CrudEF<Parent>.SearchOne("Ankit"));
+
+
+
+CrudEF<Parent>.Get().ForEach(p =>
+{
+    if (p.isActive)
+        Console.WriteLine(p.name + " : is a parent");
+    else
+    Console.WriteLine(p.name + ": is a child ");
+});
+
